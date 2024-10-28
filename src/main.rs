233b27becd1753fb8d417fb643610f3eca0acd80
rodrigo <correo.rodrigo@gmail.com>
2024-10-28 17:30:34 -0500

    health_check test

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use zero2prod::run;
+use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<(),std::io::Error> {
-    run()?.await
+    let listener = TcpListener::bind("127.0.0.1:8000");
+    run(listener?)?.await
 }
 
