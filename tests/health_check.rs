811180d532d339e375560ecbbdafe50f1385f387
rodrigo <correo.rodrigo@gmail.com>
2024-11-06 16:07:33 -0500

    logs

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -8,8 +8,24 @@ use once_cell::sync::Lazy;
 
 // Ensure thet the 'tracing' stack is only initialized once using 'one cell'
 static TRACING: Lazy<()> = Lazy::new(|| {
-    let subscriber = get_subscriber("zero2prod".into(), "info".into());
-    init_subscriber(subscriber);
+    let default_filter_level = "info".to_string();
+    let subscriber_name = "test".to_string();
+
+    if std::env::var("TEST_LOG").is_ok() {
+        let subscriber = get_subscriber(
+            subscriber_name,
+            default_filter_level,
+            std::io::stdout 
+        );
+        init_subscriber(subscriber);
+    } else {
+        let subscriber = get_subscriber(
+            subscriber_name, 
+            default_filter_level,
+            std::io::sink
+        );
+        init_subscriber(subscriber);
+    }    
 });
 
 pub struct TestApp {
