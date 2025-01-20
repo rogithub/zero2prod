415812b6a4315cbc9b879c1fc9edf13d9a94dfd5
rodrigo <correo.rodrigo@gmail.com>
2025-01-20 13:42:20 -0600

    email

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -3,7 +3,7 @@ use sqlx::PgPool;
 use uuid::Uuid;
 use chrono::Utc; 
 use unicode_segmentation::UnicodeSegmentation;
-use crate::domain::{ NewSubscriber, SubscriberName };
+use crate::domain::{ NewSubscriber, SubscriberName, SubscriberEmail };
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -28,9 +28,14 @@ pub async fn subscribe(
         Ok(name) => name,
         Err(_) => return HttpResponse::BadRequest().finish()
     };
+
+    let email: SubscriberEmail = match SubscriberEmail::parse(form.0.email) {
+        Ok(email) => email,
+        Err(_) => return HttpResponse::BadRequest().finish()
+    };
     
     let new_subscriber = NewSubscriber {
-        email: form.0.email,
+        email: email,
         name: name
     };    
 
@@ -78,7 +83,7 @@ pub async fn insert_subscriber(
         VALUES ($1, $2, $3, $4)
         "#,
             Uuid::new_v4(),
-            new_subscriber.email,
+            new_subscriber.email.as_ref(),
             new_subscriber.name.as_ref(),
             Utc::now())
     .execute(pool)
